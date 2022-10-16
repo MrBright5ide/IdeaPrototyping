98b4efd97fd8ab1d58068ba8767563070d675db4
MrBright5ide <halomodder051@gmail.com>
2022-10-15 22:59:26 -0600

    Proj1 Upload
    A Skyrim playthrough picker. Picks what race, and what skills to major in. Also says what hold you can live in.

---
+++ Proj1/lists.py
@@ -0,0 +1,25 @@
+# lists for use by the main.py program
+
+race = {"High Elf", "Argonian", "Wood Elf", "Breton", "Dark Elf", "Imperial", "Khajiit", "Nord", "Orc", "Redguard"}
+
+skill = {"Combat" : {"Conjuration",\
+		"Destruction",\
+		"Archery",\
+		"One-Handed",\
+		"Two-Handed"},\
+	"Mercantile": {"Enchanting",\
+		"Smithing",\
+		"Alchemy",\
+		"Pickpocket",\
+		"Speech"},\
+	"Passive": "Heavy Armor",\
+		"Sneak",\
+		"Light Armor"},\
+	"Misc";"Alteration",\
+		"Illusion",\
+		"Restoration",\
+		"Block",\
+		"Lockpicking"}}
+hold = {"Eastmarch", "Falkreath", "Haafingar", "Hjaalmarch", "The Pale", "The Reach", "The Rift", "Whiterun", "Winterhold"}
+
+goal = {"kill alduin","have 100,000 gold", "Become Arch-Mage", omplete the Thieves' Guild questline", "Complete the Dark Brotherhood questline", "Complete the Companions questline"}
---
+++ Proj1/main.py
@@ -0,0 +1,16 @@
+#	Project Windborn
+#	Creator: Brickman
+#	10/15/2022
+#	version 1.0
+
+import random
+import lists
+
+def main():
+
+def genClass():
+
+def button():
+	while buttonCheck != on:
+		
+main()
